6edf4d33e113f2d6f6924c98eed4adb9b6e78a5b
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-11-07 23:39:14 +0100

    Add request handling logic

--- server.rb
+++ server.rb
@@ -1,9 +1,42 @@
 require 'socket'
+Methods = {'GET' => :get_method, 'POST' => :post_method}
 
-server = TCPServer.open(2000)
+def get_method(path)
+  content = ""
+  File.open(path, 'r') do |f|
+    content = f.read
+  end
+  content
+end
+
+def create_response(path, method)
+  begin
+    content = send(method, path)
+    status = "200 OK"
+  rescue
+    status = "404 Not Found"
+  end
+
+  header = <<-RESP_HEADER
+HTTP/1.1 #{status}\r
+Date: #{Time.now.ctime}\r
+Content-Type: text/html\r
+Content-Length: #{content.length}\r\n\r
+  RESP_HEADER
+
+  header += content
+end
+
+server = TCPServer.open(8080)
 loop do
   client = server.accept
-  client.puts Time.now.ctime
-  client.puts "Closing the connection. Bye!"
+  client_request = client.gets
+  puts client_request
+  client_request =~ /([A-Z]+)[ ]\/(.+)[ ]([A-Z]+\/\d\.\d)[\r\n][\r\n]/
+  # unless Methods.include?($1)
+  #   client.puts "Illegal request. Closing connection"
+  #   client.close
+  # end
+  client.print create_response($2.to_s, Methods[$1])
   client.close
 end
