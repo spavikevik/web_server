3398e6cc745211dd085f262323be4437cb15a933
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-11-12 01:50:15 +0100

    Add GET and POST methods and logic for processing client requests; move functionality to methods

--- browser.rb
+++ browser.rb
@@ -1,13 +1,16 @@
 require 'socket'
+require 'json'
 
 host = 'localhost'
 port = 8080
 path = '/index.html'
 
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+post_data = { :viking => { :name=>"Erik the Red", :email=>"erikthered@theodinproject.com" } }.to_json
+header = "POST #{path} HTTP/1.0\r\nContent-Length: #{post_data.length}\r\n\r\n"
 
 socket = TCPSocket.open(host, port)
-socket.print(request)
+socket.print (header + post_data)
+
 response = socket.read
 
 headers, body = response.split("\r\n\r\n", 2)
--- server.rb
+++ server.rb
@@ -1,12 +1,40 @@
 require 'socket'
+require 'json'
+require 'erb'
+
 Methods = {'GET' => :get_method, 'POST' => :post_method}
+def generate_header(status, content_length=0)
+  <<-RESP_HEADER
+HTTP/1.1 #{status}\r
+Date: #{Time.now.ctime}\r
+Content-Type: text/html\r
+Content-Length: #{content_length}\r\n\r
+  RESP_HEADER
+end
+
+def vikings_html
+  html = ""
+  params['viking'].each do |k, v|
+    html += "<li>#{k.capitalize}: #{v}</li>"
+  end
+  html
+end
+
+def yield_to_template(template, replacement)
+  template.gsub("<%= yield %>", replacement)
+end
 
 def get_method(path)
-  content = ""
-  File.open(path, 'r') do |f|
-    content = f.read
-  end
-  content
+  content = File.read path
+end
+
+def post_method(path)
+  thanks = File.read "thanks.html"
+  yield_to_template thanks, vikings_html
+end
+
+def params
+  JSON.parse $post_data
 end
 
 def create_response(path, method)
@@ -14,29 +42,27 @@ def create_response(path, method)
     content = send(method, path)
     status = "200 OK"
   rescue
+    content = ""
     status = "404 Not Found"
   end
-
-  header = <<-RESP_HEADER
-HTTP/1.1 #{status}\r
-Date: #{Time.now.ctime}\r
-Content-Type: text/html\r
-Content-Length: #{content.length}\r\n\r
-  RESP_HEADER
-
-  header += content
+  generate_header(status, content.length) + content
 end
 
 server = TCPServer.open(8080)
 loop do
   client = server.accept
-  client_request = client.gets
-  puts client_request
-  client_request =~ /([A-Z]+)[ ]\/(.+)[ ]([A-Z]+\/\d\.\d)[\r\n][\r\n]/
-  # unless Methods.include?($1)
-  #   client.puts "Illegal request. Closing connection"
-  #   client.close
-  # end
+  i = 0
+  while line = client.gets and line !~ /^\s*$/
+    if i == 0
+      header = line
+    end
+    i += 1
+    if line =~ /Content-Length:\s*([\d]+)/
+      content_length = $1.to_i
+    end
+  end
+  header =~ /([A-Z]+)[ ]\/(.+)[ ]([A-Z]+\/\d\.\d)[\r\n][\r\n]/
+  $post_data = client.readpartial(content_length) if $1 == 'POST'
   client.print create_response($2.to_s, Methods[$1])
   client.close
 end
